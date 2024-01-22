3fbbcfe670b9c57751ece2b672e982219b35b0da
Guilherme Marques <guilherme.marques@turbobank.co>
2024-01-22 09:14:27 -0300

    Cria configurações comuns para APIs

--- API/Smartflix.Enrollment.API/Program.cs
+++ API/Smartflix.Enrollment.API/Program.cs
@@ -1,79 +1,21 @@
 using Enrollment.Database.Extentions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
-using System.Text;
+using SmartFlix.API.Configuration.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers()
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
-                    options.SerializerSettings.DateFormatString = "yyyy-MM-ddTHH:mm:sszzz";
-                    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-                    options.SerializerSettings.DateParseHandling = DateParseHandling.DateTimeOffset;
-                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                });
+                .ConfigNewtonsoftJson();
 
 builder.Services.AddCors();
 
-builder.Services.AddAuthentication(_ =>
-{
-    _.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    _.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(_ =>
-{
-    var key = Encoding.ASCII.GetBytes(builder.Services.BuildServiceProvider().GetService<IConfiguration>().GetValue<string>("Params:Key"));
-    _.RequireHttpsMetadata = false;
-    _.SaveToken = true;
-    _.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ClockSkew = TimeSpan.Zero,
-    };
-});
+builder.Services.ConfigJwtBearerAuthentication(builder.Configuration.GetValue<string>("Params:Key"));
 
-
-
-builder.Services.DataBaseInjections();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(_ =>
-{
-    _.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    _.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                }
-            },
-            new string[]{}
-        }
-    });
-}).AddSwaggerGenNewtonsoftSupport();
+builder.Services.AddSqlDatabaseContext(builder.Configuration["ConnectionString"])
+                .ModuleInjections()
+                .AddEndpointsApiExplorer()
+                .ConfigSwagger();
 
 var app = builder.Build();
 
@@ -96,8 +38,6 @@ app.UseCors(
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-    endpoints.MapControllers()
-);
+app.MapControllers();
 
 app.Run();
